8ce2f703d4da0ac023a60df7882c6bb07eb8ebe4
ritikdevelop <hrithikrana216@gmail.com>
2024-11-21 22:21:50 +0530

    Update global.css and tailwind.config.ts to reflect new styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <h2>
-      Hello, World!
-    </h2>
-   </div>
+    <div className="flex h-screen items-center justify-center ">
+      <h1 className="text-3xl text-brand">Hello, World!</h1>
+    </div>
   );
 }
