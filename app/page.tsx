e25eebd1d551123be631b02439476df098e4a377
ritikdevelop <hrithikrana216@gmail.com>
2024-11-21 23:14:26 +0530

    "Removed GeistMonoVF and GeistVF font files, updated globals.css and layout.tsx to use Google's Poppins font, and added a new CSS class 'center'."

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center ">
+    <div className="center">
       <h1 className="text-3xl text-brand">Hello, World!</h1>
     </div>
   );
