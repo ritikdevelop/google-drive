97ae7d09fdd16e14d1aff05acad82c48b86f6cfe
ritikdevelop <hrithikrana216@gmail.com>
2024-11-24 22:46:26 +0530

    "Updated auth layout, added AuthForm component, and installed new dependencies (@hookform/resolvers, @radix-ui/react-label, @radix-ui/react-slot, react-hook-form, and zod)"

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen">
-      <section className="bg-brand p-10">
-        <div>
+      <section className="bg-brand p-10 hidden w-1/2 items-center justify-center lg:flex xl:w-2/5">
+        <div className="flex max-h[750px] max-w[430px] flex-col justify-center space-y-8">
           <Image
-            src="/favicon.ico"
+            src="/assets/icons/logo-full.svg"
             alt="logo"
-            width={16}
-            height={16}
+            width={220}
+            height={80}
             className="h-auto"
           />
           <div className="space-y-5 text-white">
@@ -18,9 +18,28 @@ const layout = ({ children }: { children: React.ReactNode }) => {
               This is a place where you can store all your documents.
             </p>
           </div>
+          <Image
+            src="/assets/images/files.png"
+            alt="Files"
+            width={330}
+            height={330}
+            className="transition-all hover:rotate-2 hover:scale-105"
+          />
         </div>
       </section>
-      {children}
+
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-16 lg:hidden">
+          <Image
+            src="/assets/icons/logo-full-brand.svg"
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto w-[200px] lg:w-[250px]"
+          />
+        </div>
+        {children}
+      </section>
     </div>
   );
 };
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,7 @@
 "use client";
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
-const SignUp = () => {
-  return <div>SignUp</div>;
-};
+const SignUp = () => <AuthForm type="sign-up" />
 
 export default SignUp;
