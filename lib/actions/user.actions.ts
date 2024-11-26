2553c7c386b36349b7c0758074a7464452144ae6
ritikdevelop <hrithikrana216@gmail.com>
2024-11-26 22:57:47 +0530

    "Updated Appwrite client imports and added Avatars and Storage to createAdminClient"

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,12 @@
+//? Create Account Flow
+// The create account flow is a critical part of the user experience. It should be easy to follow,
+
+// 1. Users enters full name and email
+// 2. Check if the user already exist or not
+// 3. If not, create a new user
+// 4. If yes, show the login page
+// 5. Send OTP to users email
+// 6. Return the users accountId that will be used to complete the login
+// Verify OTP and authenticate to login
+
+
--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -1,32 +1,48 @@
 // node-appwrite
 
-import { Client, Account, Databases } from "node-appwrite";
+import { Client, Account, Databases, Avatars, Storage } from "node-appwrite";
 import { appwriteConfig } from "@/lib/appwrite/config";
 import { cookies } from "next/headers";
 import { get } from "http";
-export const createSessionClient = async () =>{
-    const client = new Client()
+export const createSessionClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
+
+  const session = (await cookies()).get("appwrite-session");
+  if (!session || !session.value) {
+    throw new Error("No session found");
+  }
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+export const createAdminClient = async () => {
+  const client = new Client()
     .setEndpoint(appwriteConfig.endpointUrl)
     .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
 
-
-    const session = (await cookies()).get('appwrite-session');
-    if(!session || !session.value){
-        throw new Error('No session found');
-    }
-    client.setSession(session.value);
-
-    return{
-        get account(){
-            return new Account(client);
-        }
-        get databases(){
-            return new Databases(client);
-        }
-    }
-}
-
-
-export const createAdminClient = async () =>{
-
-}
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
